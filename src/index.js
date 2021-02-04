77f0a32f0dfeaf55ce0c910464a6c70c381a4e3b
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-04 17:49:26 -0500

    commit tree 7efe53abdb37962d8afa1b34456f557b6d5eccee

--- src/index.js
+++ src/index.js
@@ -12,4 +12,86 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  const createBtn = document.getElementsByClassName("submit")[0];
+
+  createBtn.addEventListener("click", handleSubmit);
+  document
+    .getElementById("toy-collection")
+    .addEventListener("click", handleLike);
+
+  fetch("http://localhost:3000/toys")
+    .then((response) => response.json())
+    .then((data) => {
+      handleToyArray(data);
+    });
 });
+
+function handleToyArray(arr) {
+  arr.map((toy) => {
+    appendToy(toy);
+  });
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const toyInfo = {
+    name: document.getElementsByName("name")[0].value,
+    image: document.getElementsByName("image")[0].value,
+    likes: 0,
+  };
+
+  const configObj = {
+    method: "Post",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(toyInfo),
+  };
+
+  fetch("http://localhost:3000/toys", configObj)
+    .then((r) => r.json())
+    .then((json) => {
+      appendToy(json);
+    });
+}
+
+function appendToy(toy) {
+  toyCard = document.createElement("div");
+  toyCard.classList.add("card");
+  toyCard.dataset.id = `${toy.id}`;
+  toyCard.innerHTML = `
+      <h2>${toy.name}</h2>
+      <img src="${toy.image}" class="toy-avatar">
+      <p>${toy.likes} Likes</p>
+      <button class="like-btn">Like <3 </button>
+      `;
+  document.getElementById("toy-collection").append(toyCard);
+}
+
+function handleLike(e) {
+  e.preventDefault();
+  if (e.target.className != "like-btn") {
+    return;
+  }
+  parentContainer = e.target.parentElement;
+  pTag = parentContainer.children[2];
+  toyId = parseInt(parentContainer.dataset["id"]);
+
+  pTag.innerHTML = `${parseInt(pTag.innerText) + 1} Likes`;
+
+  const configObj = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      likes: `${parseInt(pTag.innerText) + 1}`,
+    }),
+  };
+
+  fetch(`http://localhost:3000/toys/${toyId}`, configObj);
+}
